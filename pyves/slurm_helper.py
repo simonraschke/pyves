defc873180c8951c7f3a8951aea5edfcf2cdff77
Simon Raschke <simon.raschke@outlook.de>
2021-05-14 20:46:08 +0200

    added mkdir to sbatchController

--- pyves/slurm_helper.py
+++ pyves/slurm_helper.py
@@ -274,8 +274,86 @@ def sbatchController(
     controller = "pyves.Controller.Static",
     controller_kwargs = dict(analysis=True),
     dryrun = False,
+    log = True,
+    mode = "new",
+    mkdir = True,
     modules = None
 ):
+    distutils.log.set_verbosity(distutils.log.DEBUG)
+    # prmsfilename = "parameters.json"
+
+    if log: print(f"mode is {mode}")
+
+    
+    if mode == "new":
+        # make new directory
+        dirpath = os.path.realpath(dirpath)
+        if log: print("trying dir:", dirpath)
+        if os.path.exists(dirpath):
+            dirpath += "_0"
+            if log: print("exists. trying dir:", dirpath)
+        _it = 1
+        while os.path.exists(dirpath):
+            dirpath = dirpath.rsplit("_",1)[0] + f"_{_it}"
+            if log: print("exists. trying dir:", dirpath)
+            _it += 1
+        if log: print("valid path:", dirpath)
+        
+
+        if mkdir and not dryrun:
+            if log: print("mkdir:", dirpath)
+            os.makedirs(dirpath)
+
+        # verify that there is a directory
+        if not mkdir and not dryrun:
+            if not os.path.exists(dirpath):
+                raise OSError(f"{dirpath} does not exist")
+
+        
+        # make new parameters.json from dict
+        if isinstance(prms, type({})):
+            if log: print("prms is given. will ignore \"prmspath\"")
+            prmspath = os.path.realpath(os.path.join(dirpath, prmsfilename))
+                
+            if Path(prmspath).is_file():
+                if log: print(f"save the old parameter file {prmspath}")
+                distutils.file_util.copy_file(prmspath, os.path.join(dirpath, "parameters_old.json"), verbose=1)
+            
+            setup_prms = default_prms
+            setup_prms.update(prms)
+            with open(prmspath, 'w') as fp:
+                json.dump(setup_prms, fp=fp, indent=4)
+
+
+        # copy existing *.json to parameters.json
+        elif Path(os.path.realpath(prmspath)).is_file():
+            prmspath = os.path.realpath(prmspath)
+            
+            if Path(prmspath).is_file():
+                if log: print(f"save the old parameter file {os.path.join(dirpath, prmsfilename)}")
+                if Path(os.path.join(dirpath, prmsfilename)).is_file():
+                    distutils.file_util.copy_file(os.path.join(dirpath, prmsfilename), os.path.join(dirpath, "parameters_old.json"), verbose=1)
+            
+            if log: print(f"copy the input file")
+            distutils.file_util.copy_file(prmspath, dirpath, verbose=1)
+
+        else:
+            raise IOError("neither prms nor prmspath were given")
+
+
+        
+    elif mode == "change":
+        
+        if Path(os.path.realpath(prmspath)).is_file():
+            prmspath = os.path.realpath(prmspath)
+            dirpath = os.path.realpath(dirpath)
+        elif Path(os.path.join(dirpath, prmspath)).is_file():
+            prmspath = os.path.join(dirpath, prmspath)
+        else:
+            raise IOError(f"No parameter file at {prmspath}")
+
+
+
     # check if sbatch exists on system
     sbatchpath = which("sbatch")
     # print("sbatchpath", sbatchpath)
